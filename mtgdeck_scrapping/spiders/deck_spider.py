c69d75241a29cc6313b0848aeb06aa18efd15058
Ludovic CWI <ludovic@chanewon.in>
2016-07-01 08:26:27 +0200

    dev : print deck and configure slow throttle

--- mtgdeck_scrapping/spiders/deck_spider.py
+++ mtgdeck_scrapping/spiders/deck_spider.py
@@ -6,7 +6,7 @@ class DeckSpider(scrapy.Spider):
     name = "deck"
     allowed_domains = ["starcitygames.com"]
     start_urls = [
-        "http://sales.starcitygames.com/deckdatabase/deckshow.php?t%5BT1%5D=1&event_ID=&feedin=&start_date=04%2F10%2F2016&end_date=07%2F03%2F2016&city=&state=&country=&start=&finish=&exp=&p_first=&p_last=&simple_card_name%5B1%5D=&simple_card_name%5B2%5D=&simple_card_name%5B3%5D=&simple_card_name%5B4%5D=&simple_card_name%5B5%5D=&w_perc=0&g_perc=0&r_perc=0&b_perc=0&u_perc=0&a_perc=0&comparison%5B1%5D=%3E%3D&card_qty%5B1%5D=1&card_name%5B1%5D=&comparison%5B2%5D=%3E%3D&card_qty%5B2%5D=1&card_name%5B2%5D=&comparison%5B3%5D=%3E%3D&card_qty%5B3%5D=1&card_name%5B3%5D=&comparison%5B4%5D=%3E%3D&card_qty%5B4%5D=1&card_name%5B4%5D=&comparison%5B5%5D=%3E%3D&card_qty%5B5%5D=1&card_name%5B5%5D=&sb_comparison%5B1%5D=%3E%3D&sb_card_qty%5B1%5D=1&sb_card_name%5B1%5D=&sb_comparison%5B2%5D=%3E%3D&sb_card_qty%5B2%5D=1&sb_card_name%5B2%5D=&card_not%5B1%5D=&card_not%5B2%5D=&card_not%5B3%5D=&card_not%5B4%5D=&card_not%5B5%5D=&order_1=finish&order_2=&limit=25&action=Show+Decks&p=1"
+        "http://sales.starcitygames.com//deckdatabase/deckshow.php?&t%%5BC1%%5D=1&start_date=06/26/2016&end_date=07/03/2016&start_num=%s&limit=10" % start_num for start_num in xrange(0, 21, 10)
     ]
 
     def parse(self, response):
@@ -20,9 +20,10 @@ class DeckSpider(scrapy.Spider):
             deck['format'] = deckTr.xpath('td[5]/text()').extract()
             deck['date'] = deckTr.xpath('td[6]/a/text()').extract()
             deck['location'] = deckTr.xpath('td[7]/a/text()').extract()
-            deck['cards'] = []
-            deck_absolute_url = response.urljoin(deck['url'])
-            yield scrapy.Request(deck_absolute_url, callback=self.parse_deck, meta={'deck': deck})
+            deck['maincards'] = []
+            print deck
+            #deck_absolute_url = response.urljoin(deck['url'])
+            #yield scrapy.Request(deck_absolute_url, callback=self.parse_deck, meta={'deck': deck})
 
     def parse_deck(self, response):
         deck = response.meta['deck']
@@ -31,6 +32,6 @@ class DeckSpider(scrapy.Spider):
             card = CardItem()
             card['occurrence'] = card_wrapper.xpath('text()')
             card['name'] = card_wrapper.xpath('/a/text()')
-            deck['cards'].append(card)
+            deck['maincards '].append(card)
 
         print deck
